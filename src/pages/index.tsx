a68906463b18bdc119852889aed3e47c6229e54f
pivattoo <pivattogui@gmail.com>
2022-06-22 21:10:54 -0300

    wip: home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,7 @@
-const technologies = ["JavaScript", "Next.js", "Prisma"]
+const technologies = [
+  { name: "JavaScript", function: "Front-end, Back-end" },
+  { name: "Next.js", function: "Framework" },
+  { name: "Prisma", function: "ORM" }]
 
 export default function Home() {
   return (
@@ -12,22 +15,23 @@ export default function Home() {
       </div>
       <div className="font-semibold text-gray-800">
         <h1 className="text-4xl pb-4">Rpg Manager</h1>
-        <label className="pl-2">Gerenciador genérico para RPGs.</label>
+        <label className="pl-2">Gerenciador genérico para RPGs</label>
         <div className="pl-2 pt-4">
           <span>Tecnologias utilizadas no projeto:</span>
-          <div className="border-slate-200 border w-1/3 bg-white shadow rounded-md block overflow-auto">
+          <div className="mt-2 border-gray-200 border w-2/3 bg-white shadow rounded-md block overflow-auto">
             <table className="table-auto w-full">
               <thead>
-                <tr className="bg-slate-100 text-sm text-slate-800 text-left border-b border-slate-200">
+                <tr className="bg-gray-100 text-sm text-slate-800 text-left border-b border-slate-200">
                   <th className="px-4 py-4 font-medium">Tecnologias</th>
+                  <th className="px-4 py-4 font-medium"></th>
                 </tr>
               </thead>
               <tbody>
                 {technologies.map(((techology, i) => (
-                  <tr key={i} className="px-4 h-8 text-base">
-                    <td className="px-4 py-2">✅{techology}</td>
+                  <tr key={i} className="px-4 h-8">
+                    <td className="px-4 py-2">✅{techology.name}</td>
+                    <td className="px-4 py-2">{techology.function}</td>
                   </tr>
-
                 )))}
               </tbody>
             </table>
